e66d98c5e5214b94f8a612a91820e89bc8ae76a2
serixog865 <serixog865@ops.org>
2016-10-01 09:40:50 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 function problemOne() {
   //Get all the numbers from the form.
   let num1 = parseFloat(document.getElementById('num1').value);
-
+  let num2 = parseFloat(document.getElementById('num2').value);
+  let num3 = parseFloat(document.getElementById('num3').value);
+  let num4 = parseFloat(document.getElementById('num4').value);
+  let num5 = parseFloat(document.getElementById('num5').value);
+  let num6 = parseFloat(document.getElementById('num6').value);
+  
   //Find the average, not including the lowest score.
-
+  var lowScore = Math.min(num1, num2, num3, num4, num5, num6);
+  var average = ((num1 + num2 + num3 + num4 + num5 + num6 - lowScore)/5);
+  
   //Print it to the console.
   console.log(average);
 }
@@ -11,14 +18,28 @@ function problemOne() {
 function problemTwo() {
   //Get all the letter grades from the form.
   let grade1 = document.getElementById('grade1').value;
+  let grade2 = document.getElementById('grade2').value;
+  let grade3 = document.getElementById('grade3').value;
+  let grade4 = document.getElementById('grade4').value;
+  let grade5 = document.getElementById('grade5').value;
+  let grade6 = document.getElementById('grade6').value;
+  let grade7 = document.getElementById('grade7').value;
 
   //Compute the GPA.
-
+  var gradeTotal = findGPA(grade1, grade2, grade3, grade4, grade5, grade6, grade7);
+  
   //Print it to the console.
-
+  console.log(gradeTotal);
+  
+  
   //If it is 3.0 or above, turn the div with id="box" green. Otherwise, turn it red.
   // document.getElementById("box").style.backgroundColor = "orange";
-
+  if (gradeTotal >= 3.0) {
+	  document.getElementById("box").style.backgroundColor = "green";
+  }
+  else {
+  document.getElementById("box").style.backgroundColor = "red";
+  }
 }
 
 function findMin(num1, num2, num3, num4, num5, num6) {
@@ -34,12 +55,12 @@ function findMin(num1, num2, num3, num4, num5, num6) {
   return list[minimum];
 }
 
-function findGPA(grade1, grade2, grade3, grade4, grade5, grade6) {
-  let list = [grade1, grade2, grade3, grade4, grade5, grade6];
+function findGPA(grade1, grade2, grade3, grade4, grade5, grade6, grade7) {
+  let list = [grade1, grade2, grade3, grade4, grade5, grade6, grade7];
 
   let sum = 0;
 
-  for(let i = 0; i < 6; i++) {
+  for(let i = 0; i < 7; i++) {
     if(list[i] == 'a' || list[i] == 'A') {
       sum += 4;
     }
@@ -54,5 +75,5 @@ function findGPA(grade1, grade2, grade3, grade4, grade5, grade6) {
     }
   }
 
-  return sum / 6;
+  return sum / 7;
 }
